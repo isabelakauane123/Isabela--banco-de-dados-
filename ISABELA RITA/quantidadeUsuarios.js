21b692067bd1c49fad0196fe46722a01504665bd
isabela <110407436+isabelakauane123@users.noreply.github.com>
2024-10-04 10:31:54 -0300

    Add files via upload

---
+++ ISABELA RITA/quantidadeUsuarios.js
@@ -0,0 +1,23 @@
+// código omitido
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+      xaxis: {
+      
+      },
+      yaxis: {
+      
+      }
+  }
+  
+  // código omitido
